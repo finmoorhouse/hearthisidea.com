25eab225ddc9cc3cfd1ec203347cd85c64751d56
Fin Moorhouse <5040505+finmoorhouse@users.noreply.github.com>
2020-04-26 14:56:44 +0100

    Added Toke episode write-up and fixed post ordering.

--- src/components/episode-link.js
+++ src/components/episode-link.js
@@ -4,7 +4,7 @@ const PostLink = ({ post }) => (
   <div className='episode-link'>
     <Link to={post.frontmatter.path}>
 
-      <h2><span className='number'>{post.frontmatter.number}. </span>{post.frontmatter.title} </h2>
+      <h2><span className='number'>{parseInt(post.frontmatter.number, 10)}. </span>{post.frontmatter.title} </h2>
       <h4 className='date'>
       {post.frontmatter.date.toUpperCase()}
       </h4>
