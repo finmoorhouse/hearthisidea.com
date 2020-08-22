a641f808aed8cbdf48d6896fe6038ad5cd654b4a
Fin Moorhouse <5040505+finmoorhouse@users.noreply.github.com>
2020-08-22 19:52:47 +0100

    Added Coil monetization.

--- src/components/seo.js
+++ src/components/seo.js
@@ -97,6 +97,10 @@ function SEO({ description, lang, meta, title, myFeaturedImage }) {
           name:`image`,
           content: pageThumbnail,
         },
+        {
+          name:`monetization`,
+          content: "$ilp.uphold.com/4rHyDzRz66Dw",
+        },
       ].concat(meta)}
       
     />
