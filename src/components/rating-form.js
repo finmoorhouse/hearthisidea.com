92a27895b8b7c8ce36ca3a468d408f0c83579b4b
finmoorhouse <5040505+finmoorhouse@users.noreply.github.com>
2020-11-17 21:24:05 +0000

    Added a different start picker, but without half stars. Oh well.

--- src/components/rating-form.js
+++ src/components/rating-form.js
@@ -1,22 +1,20 @@
 import React, { useState, useEffect } from "react"
-import StarPicker from "react-star-picker"
+//import StarPicker from "react-star-picker"
+import StarRatingComponent from "react-star-rating-component"
 import "../styles/rating-form.scss"
 
-
 const Rate = props => {
-useEffect(()=>{
-  import ("react-star-picker")
-  .then((StarPicker) => {
-    this.StarPicker = StarPicker;
-  })
-  .catch((error) => console.error(error));
-
-
-})
+  /*
   const [rating, setRating] = useState(null)
 
   const onChange = value => {
     setRating(value)
+  }
+  */
+  const [starRating, setStarRating] = useState(null)
+
+  const onStarChange = value => {
+    setStarRating(value)
   }
 
   return (
@@ -27,18 +25,30 @@ useEffect(()=>{
       data-netlify="true"
       className="rating-form"
     >
-      <hr class='rating-hr'/>
+      <hr class="rating-hr" />
       <input type="hidden" name="form-name" value="article-rating" />
       <input type="hidden" name="episode" value={props.episode} />
-
       <label for="stars">Your rating:</label>
-      <input type="hidden" name="stars" value={rating} />
-      <StarPicker onChange={onChange} value={rating} halfStars="true" />
-      <details> 
-      <summary>Further comments</summary>
-        <textarea name="comments" rows="4" ></textarea>
+      <input type="hidden" name="stars" value={starRating} />
+  
+      {/*<StarPicker onChange={onChange} value={rating|| 0} halfStars="true" />*/}
+      <br/>
+   
+      <StarRatingComponent
+        starColor="gold"
+        name="rate1"
+        emptyStarColor="#dddddd"
+        starCount={5}
+        value={starRating}
+        onStarClick={onStarChange}
+      />
+      <details>
+        <summary>Further comments</summary>
+        <textarea name="comments" rows="4"></textarea>
       </details>
-      <button class='rating-button' type="submit">📨 Send Feedback</button>
+      <button class="rating-button" type="submit">
+        📨 Send Feedback
+      </button>
     </form>
   )
 }
