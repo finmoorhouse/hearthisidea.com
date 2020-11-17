e37789828af6391c256e9ffd36e5d8f2598a79b1
finmoorhouse <5040505+finmoorhouse@users.noreply.github.com>
2020-11-17 16:54:05 +0000

    Tried adding a hidden field in the Netlify form.

--- src/components/rating-form.js
+++ src/components/rating-form.js
@@ -2,22 +2,25 @@ import React from "react"
 
 const Rate = props => {
   return (
-    <form action="/thanks-for-rating" name="rating" method="POST" data-netlify="true">
+    <form
+      action="/thanks-for-rating"
+      name="Article Rating"
+      method="POST"
+      data-netlify="true"
+    >
+      <input type="hidden" name="form-name" value="rate" />
+
       <label for="rating">Your rating:</label>
-      <select type="text" id="rating" name="rating" >
-          <option value="1">⭐</option>
-          <option value="2">⭐⭐</option>
-          <option value="3">⭐⭐⭐</option>
-          <option value="4">⭐⭐⭐⭐</option>
-          <option value="5">⭐⭐⭐⭐⭐</option>
+      <select type="text" id="rating" name="rating">
+        <option value="1">⭐</option>
+        <option value="2">⭐⭐</option>
+        <option value="3">⭐⭐⭐</option>
+        <option value="4">⭐⭐⭐⭐</option>
+        <option value="5">⭐⭐⭐⭐⭐</option>
       </select>
       <label for="comments">Further comments:</label>
-      <textarea name="comments" rows="4" cols="50">
-
-      </textarea>
-      <button type="submit">
-        Go
-      </button>
+      <textarea name="comments" rows="4" cols="50"></textarea>
+      <button type="submit">Go</button>
     </form>
   )
 }
