991e9c0ac806abd9b1b1e2a0c6acd91e4191cdf0
finmoorhouse <5040505+finmoorhouse@users.noreply.github.com>
2020-11-29 15:55:13 +0000

    Published Luke Freeman episode.

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,6 +9,7 @@ const path = require(`path`)
 exports.createPages = async ({ actions, graphql, reporter }) => {
   const { createPage } = actions
   const blogPostTemplate = path.resolve(`src/templates/episode.js`)
+  const blogListTemplate = path.resolve(`src/pages/episodes.js`)
   const result = await graphql(`
     {
       allMdx(
@@ -30,6 +31,22 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
     reporter.panicOnBuild(`Error while running GraphQL query.`)
     return
   }
+  // const posts = result.data.allMdx.edges
+  // const postsPerPage = 5;
+  // const numPages = Math.ceil(posts.length / postsPerPage);
+  // Array.from({ length: numPages }).forEach((_, i) => {
+  //   createPage({
+  //     path: i === 0 ? `/` : `/${i + 1}`,
+  //     component: blogPostTemplate,
+  //     context: {
+  //       limit: postsPerPage,
+  //       skip: i * postsPerPage,
+  //       numPages,
+  //       currentPage: i + 1,
+  //     },
+  //   });
+  // });
+
   result.data.allMdx.edges.forEach(({ node }) => {
     createPage({
       path: node.frontmatter.path,
