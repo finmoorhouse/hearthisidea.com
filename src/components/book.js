e3d13187ea164a169a634bd8b4b58cd803a895e2
finmoorhouse <5040505+finmoorhouse@users.noreply.github.com>
2021-02-01 10:31:06 +0000

    Added 3d book effect.

--- src/components/book.js
+++ src/components/book.js
@@ -6,11 +6,17 @@ import "../styles/bookshelf.scss"
 const Book = props => {
   const mySrc = "/book-covers/".concat(props.image).concat(".jpg")
   return (
-    <div className="book">
+    
+    <div className={props.spineColor ? 'book book-3d' : 'book book-no3d'} >
       <a href={props.url} target="_blank" rel="noopener noreferrer">
         <img src={mySrc} objectfit="cover" objectPosition="50% 50%" alt="" />
       </a>
+      {
+        props.spineColor &&  <div className='afterBook' style={{backgroundColor: props.spineColor,}}></div>
+      }
+   
     </div>
+    
   )
 }
 
