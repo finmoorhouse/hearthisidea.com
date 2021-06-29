a09d40e9127f76824fec78223a1d8de8ade05257
finmoorhouse <5040505+finmoorhouse@users.noreply.github.com>
2021-06-29 10:31:52 +0100

    Mentioned our grant from EA Funds.

--- src/pages/donate.js
+++ src/pages/donate.js
@@ -9,8 +9,10 @@ const DonatePage = () => (
     <h1>Leave a tip</h1>
     <hr className="line" />
     <p className="introduction">
-      Thank you for considering being a supporter of the show! We produce this podcast on our own time, and episodes can take between 10-20 hours to complete.
-      We do not and never will run ads, so your donation will help us pay for hosting and invest in new equipment (and caffeine ☕).
+      Thank you for considering being a supporter of the show! We produce this
+      podcast on our own time, and episodes can take between 10-20 hours to
+      complete. We do not and never will run ads, so your donation will help us
+      pay for hosting and invest in new equipment (and caffeine ☕).
     </p>
     <div className="listen-container">
       <a
@@ -19,9 +21,18 @@ const DonatePage = () => (
         target="_blank"
         rel="noopener noreferrer"
       >
-       💸 Donate
+        💸 Donate
       </a>
     </div>
+    <p>
+      Special thank you to the <a href='https://funds.effectivealtruism.org/funds/ea-community' target='_blank'>EA Infrastructure Fund</a> for supporting us with a{" "}
+      <a
+        href="https://funds.effectivealtruism.org/funds/payouts/november-2020-ea-infrastructure-fund-grants#hear-this-idea---%C2%A36000-8000"
+        taregt="_blank"
+      >
+        grant in December 2020.
+      </a> This covered our time as we worked on the podcast over the pandemic. If you have a project you think could improve the world, we highly recommend thinking about <a href='https://funds.effectivealtruism.org/apply-for-funding' target='_blank'>applying to an EA Fund</a>!
+    </p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}></div>
   </Layout>
 )
