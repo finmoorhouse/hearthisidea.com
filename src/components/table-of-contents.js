cee3a3b3c12e0e64a16d19c56ee91b107330a094
jonas-kgomo <jonaskgmoo@gmail.com>
2022-05-28 20:06:00 +0100

    anchor link replacement for a tag

--- src/components/table-of-contents.js
+++ src/components/table-of-contents.js
@@ -1,27 +1,30 @@
 import React from "react"
 //import { useStaticQuery, graphql } from "gatsby"
+import { AnchorLink } from "gatsby-plugin-anchor-links";
+
 function renderItems(items, level) {
-  return (
-    <ol>
-      {items.map(
-        item =>
-          item.title && (
-            <li key={item.url}>
-              <a href={item.url}>{item.title}</a>
-              {item.items && item.title && level <= 3 && renderItems(item.items, level + 1)}
-            </li>
-          )
-      )}
-    </ol>
-  )
+  return (<ol>
+    {items.length > 0 && items.map(
+      item =>
+      (<li key={item.url}>
+        <AnchorLink to={`/episodes/crawford/${item.url}`} title="references titles">
+          <span>{item.title}</span>
+        </AnchorLink>
+        {item.items && item.title && level <= 3 && renderItems(item.items, level + 1)}
+      </li>)
+    )}
+  </ol>)
 }
 const TableOfContents = props => {
   return (
-    <details open className='episode-table-of-contents'>
-      <summary>Table of Contents</summary>
-      {renderItems(props.items, 1)}
-      
-    </details>
+    <div>
+      <details open className='episode-table-of-contents'>
+        <summary>Table of Contents</summary>
+        {renderItems(props.items, 1)}
+
+      </details>
+    </div>
+
   )
 }
 
